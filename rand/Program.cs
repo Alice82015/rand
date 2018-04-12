c7586ef38c3d8995c19f8382e4fa5329fba2ca00
m305-9\w401 <a106082015@mail.shu.edu.tw>
2018-04-12 17:37:08 +0800

    作業完成YA

--- rand/Program.cs
+++ rand/Program.cs
@@ -10,6 +10,33 @@ namespace rand
     {
         static void Main(string[] args)
         {
+            // 1.將所有號碼都放入list中
+            List<int> numbers = new List<int>();
+            for(int i = 1; i <=42; i++)
+            {
+                numbers.Add(i);
+            }
+
+            // 產生亂數物件
+            Random rand = new Random();
+
+            // 重複動作
+            for (int i = 0; i < 6; i++)
+            {
+                // 隨機挑選其中一個出來
+                int r = rand.Next(1, numbers.Count);
+                Console.Write(numbers[r] + " ");
+
+                // 從list中刪除
+                numbers.RemoveAt(r);
+            }
+
+            // 暫停螢幕
+            Console.ReadLine();
+            
+
+            
+
         }
     }
 }
